622ae82f5dde5d52eac4a2f2fb64bbd1f92aa356
markxu <15136723391@sina.cn>
2017-12-26 19:27:30 +0800

    添加兴趣测评

--- Scripts/js/AssessmentIndex.js
+++ Scripts/js/AssessmentIndex.js
@@ -11,6 +11,70 @@ $(function(){
 	    });
 	}());
 	
+	//答题进度
+	(function(init,num){
+		var radialObj = radialIndicator('#indicatorContainer', {
+	        barColor: '#ffbf00',
+	        barWidth: 9,
+	        initValue: init,
+	        minValue:0,
+    		maxValue: num,
+	        fontFamily:'"PingFang SC","Microsoft Yahei"',
+	        fontWeight:'normal',
+	        fontSize:28,
+	        fontColor:"#fff",
+	        roundCorner : true,
+	        percentage: false,
+	        format: function (value) {
+        		return value+"/"+num;
+    		}
+	   	});
+		
+		setInterval(function () {
+			if(init===61){
+				init=0;
+			}
+    		radialObj.value(init++);
+		}, 1000);
+	}(window.assessmentInit=0,window.assessmentNum=60));
+	
+	//测评报告
+	(function(){
+		$('#container').highcharts({
+	        chart: {
+	            polar: true,
+	            type: 'area'
+	        },
+	        title: {
+	            text: '职业兴趣测评',
+	            x: -80
+	        },
+	        pane: {
+	            size: '80%'
+	        },
+	        xAxis: {
+	            categories: ['R', 'I', 'A', 'S',
+	                         'E', 'C'],
+	            tickmarkPlacement: 'on',
+	            lineWidth: 0
+	        },
+	        yAxis: {
+	            gridLineInterpolation: 'polygon',
+	            lineWidth: 0,
+	            min: 0
+	        },
+	        legend: {
+	        	enabled:false
+	        },
+	        series: [{
+	            name:'分数',
+	            showInNavigator:false,
+	            data: [43000, 59000, 60000, 35000, 44000, 60000],
+	            pointPlacement: 'on'
+	        }]
+	    });
+	}());
+	
 	//事件注册
 	(function(){
 		
@@ -18,27 +82,20 @@ $(function(){
 			$('body,html').animate({scrollTop:0},300);
 		})
 		
-		$("body").on("click",".position-name",function(){
-			window.location.href="Index.html";
+		$("body").on("click",".assessment-reset-btn",function(){
+			$(".content-container").hide();
+			$(".assessment-report").show();
 		})
 		
-		$("body").on("click",".sort-item",function(){
-			var _self=this;
-			if(!$(_self).hasClass(".sort-item-change")){
-				$(".sort-item").each(function(i,obj){
-					$(obj).removeClass("sort-item-change");
-				})
-				$(_self).addClass("sort-item-change");
-			}else{
-				
-			}
-		})
-		
-		$("body").on("click",".position-like",function(){
-			$(this).find(".like-icon").find(".fa-heart-o").hide();
-			$(this).find(".like-icon").find(".fa-heart").show();
+		$("body").on("click",".assessment-submit-btn",function(){
+			console.log(123);
+			$(".content-container").hide();
+			$(".prompt-login").show();
 		})
 		
 	}());
-	
 })
+
+
+
+
